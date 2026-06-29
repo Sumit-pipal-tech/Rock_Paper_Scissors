21db47260e6996e42799075dee76d11c6e7a8d75
SUMIT <sumit.pipal.cse@gmail.com>
2026-06-29 18:38:11 +0530

    Add playRound function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+console.log("Script loaded");
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
 
@@ -17,5 +21,64 @@ function getHumanChoice() {
     return choice.toLowerCase();
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log("It is a tie!");
+    }
+    else if (humanChoice === "rock") {
+
+        if (computerChoice === "scissors") {
+            humanScore++;
+            console.log("You win! Rock beats Scissors");
+        }
+
+        else {
+            computerScore++;
+            console.log("You lose! Paper beats Rock");
+        }
+
+    }
+
+    else if (humanChoice === "paper") {
+
+        if (computerChoice === "rock") {
+            humanScore++;
+            console.log("You win! Paper beats Rock");
+        }
+
+        else {
+            computerScore++;
+            console.log("You lose! Scissors beats Paper");
+        }
+
+    }
+
+    else if (humanChoice === "scissors") {
+
+        if (computerChoice === "paper") {
+            humanScore++;
+            console.log("You win! Scissors beats Paper");
+        }
+
+        else {
+            computerScore++;
+            console.log("You lose! Rock beats Scissors");
+        }
+
+    }
+
+
+    console.log("Human Score :", humanScore);
+    console.log("Computer Score:", computerScore);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log("Human:", humanSelection);
+console.log("Computer:", computerSelection);
+
+playRound(humanSelection, computerSelection);
